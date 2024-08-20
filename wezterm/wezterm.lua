599ddb90fea563c7580aa57398dc2c98f7a4283d
bwilliams <bwilliams@commsys.com>
2024-08-20 13:35:44 -0400

    updated wezterm launch menu config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local action = wezterm.action
 local config = wezterm.config_builder()
 local picture_path = "C:\\Users\\bwilliams\\Pictures\\"
 local utils = require("utils.utils")
+local home = os.getenv("HOME") and os.getenv("HOME") or os.getenv("HOMEPATH")
 
 -- Other modules for wezterm
 require("tabs").setup(config)
@@ -14,6 +15,10 @@ require("keys").setup(config)
 config.automatically_reload_config = true
 config.default_prog = { "nu.exe" }
 config.default_cwd = utils.checkDir("D:/CommSys") and "D:/CommSys" or "D:/Github"
+config.launch_menu = {
+  { label = "Wezterm Config", args = { "nvim", home .. "/.config/wezterm/" } },
+  { label = "Dev Drive", args = { "yazi", "D:/CommSys" } },
+}
 
 local eldritch_wallpaper = utils.file_exists(picture_path .. "Eldritch1920x1080.png") and "Eldritch1920x1080.png" or "eldritch.png"
 
@@ -45,6 +50,27 @@ config.underline_thickness = 3
 config.underline_position = -6
 
 if wezterm.target_triple:find("windows") then
+  table.insert(config.launch_menu, { label = "PowerShell", args = { "pwsh.exe", "-NoLogo" } })
+
+  -- Find installed visual studio version(s) and add their compilation
+  -- environment command prompts to the menu
+  for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files")) do
+    local year = vsvers:gsub("Microsoft Visual Studio/", "")
+    local archs = { "amd64", "x86" }
+
+    for _, arch in ipairs(archs) do
+      table.insert(config.launch_menu, {
+        label = (arch == "amd64" and "x64" or "x86") .. " Native Tools Developer Command Prompt " .. year,
+        args = {
+          "cmd.exe",
+          "/k",
+          "C:/Program Files/" .. vsvers .. "/Professional/Common7/Tools/VsDevCmd.bat",
+          "-arch=" .. arch,
+        },
+      })
+    end
+  end
+
   wezterm.on("gui-startup", function(cmd)
     local screen = wezterm.gui.screens().active
     local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
@@ -77,5 +103,12 @@ config.command_palette_bg_color = "#323449"
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 2 }
 config.integrated_title_buttons = { "Maximize", "Close" }
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+
+-- Hyperlink configs
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+table.insert(config.hyperlink_rules, {
+  regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+  format = "https://github.com/$1/$3",
+})
 
 return config
