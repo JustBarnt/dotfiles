26a23cab624a0a6d5a3c22ab5ee92aff8884d0eb
justbarnt <functnal@gmail.com>
2025-12-05 16:59:12 -0500

    updates

--- Powershell/Jira/Get-ProjectChanges.ps1
+++
@@ -1,9 +0,0 @@
-<#
-.SYNOPSIS
-  Updates the `Fix Version` and `Affected Version` columns for all `Done` items returned from the JIRA JQL Query
-
-.DESCRIPTION
-  
-#>
-
-$ErrorActionPreference = 'Stop'
--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 $env:TERM = "xterm-245color"
 
-[Diagnostic.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
 $jiracreds = New-Object System.Management.Automation.PSCredential($ENV:JIRA_USER, (ConvertTo-SecureString $ENV:JIRA_TOKEN -AsPlainText -Force)) 
 
 # enable starship
