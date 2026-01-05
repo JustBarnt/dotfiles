87e253e4cedf037702bb470b4ff68028042e3608
justbarnt <functnal@gmail.com>
2026-01-05 12:04:58 -0500

    feat: change to oh-my-posh instead of starship

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,8 @@ Import-Module GithubReleases
 $jiracreds = New-Object System.Management.Automation.PSCredential($ENV:JIRA_USER, (ConvertTo-SecureString $ENV:JIRA_TOKEN -AsPlainText -Force)) 
 
 # enable starship
-Invoke-Expression (&starship init powershell)
+# Invoke-Expression (&starship init powershell)
+# enable oh-my-posh
+oh-my-posh init pwsh | Invoke-Expression
 
 Set-Alias vim nvim
