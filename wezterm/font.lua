9490a72b9c162e735e2acbbb3227a7916187f3ac
justbarnt <functnal@gmail.com>
2026-05-14 09:07:33 -0400

    font changes

--- wezterm/font.lua
+++ wezterm/font.lua
@@ -3,16 +3,19 @@ local M = {}
 
 ---@class config: Config
 function M.setup(config)
-  config.font_size = 10.25
+  config.font_size = 11.5
 
   if wezterm.target_triple:find("darwin") then
     config.font_size = 16
   end
-
   config.freetype_load_target = "Light"
   config.freetype_load_flags = "NO_HINTING|FORCE_AUTOHINT"
   config.font = wezterm.font_with_fallback({
-    { family = "MonoLisaCustom Nerd Font", weight = "Medium" },
+    {
+      family = "Lilex Nerd Font Mono", 
+      weight = "Medium",
+      harfbuzz_features = { 'ss02', 'cv08', 'zero', 'cv15', 'cv06' }
+    },
     { family = "SymbolsNerdFontMono" }
   })
   config.bold_brightens_ansi_colors = "BrightAndBold"
