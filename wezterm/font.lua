321dbdab7e89a611bf82f3d0c8de12142abb16d5
justbarnt <functnal@gmail.com>
2026-09-08 16:45:55 -0400

    changed fonts

--- wezterm/font.lua
+++ wezterm/font.lua
@@ -12,7 +12,7 @@ function M.setup(config)
   config.freetype_load_flags = "NO_HINTING|FORCE_AUTOHINT"
   config.font = wezterm.font_with_fallback({
     {
-      family = "LythMono Nerd Font", 
+      family = "MonoLisaCustom Nerd Font", 
       weight = "Medium",
       harfbuzz_features = { 'ss02', 'cv08', 'zero', 'cv15', 'cv06' }
     },
